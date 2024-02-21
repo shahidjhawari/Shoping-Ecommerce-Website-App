5c306eab34b8f473d164fa192c982c0ff6a2b3a3
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-02-21 18:50:10 +0500

    back to local host connection

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$con=mysqli_connect("sql113.infinityfree.com","if0_35572792","1c0ZcgWg6Zd9Z","if0_35572792_ecom10");
+$con=mysqli_connect("localhost","root","","ecom10");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/php/ecom/');
-define('SITE_PATH','http://alnafeh.wuaze.com/php/ecom/');
+define('SITE_PATH','http://127.0.0.1/php/ecom/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
