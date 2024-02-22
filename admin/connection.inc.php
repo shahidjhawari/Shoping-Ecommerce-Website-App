a48854ff99764cdb0c7f4f1ca69673abd96755f6
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-02-22 20:53:03 +0500

    adding sub categ files for admin

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","ecom10");
+$con=mysqli_connect("localhost","root","","ecom");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/php/ecom/');
 define('SITE_PATH','http://127.0.0.1/php/ecom/');
 
