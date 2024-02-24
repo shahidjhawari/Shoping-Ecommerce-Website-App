30db1dbc8fc03032108ed97505eab1f276eb35e3
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-02-24 09:23:48 +0500

    latest rmoe vender from admin

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","ecom1010");
+$con=mysqli_connect("localhost","root","","ecom30");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/php/ecom/');
 define('SITE_PATH','http://127.0.0.1/php/ecom/');
 
